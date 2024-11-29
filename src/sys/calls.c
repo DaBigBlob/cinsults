e6ffb9bac4b1cba3426b973b4c976cc496127aad
Person <localboxcrox@gmail.com>
2024-11-28 19:59:34 -0500

    stuff

--- src/sys/calls.c
+++ src/sys/calls.c
@@ -37,11 +37,77 @@
         }
     #endif
 
-    // #define __x86_64__
     #ifdef __x86_64__
         /*
             https://github.com/swiftlang/swift/blob/main/docs/ABI/CallConvSummary.rst
             https://book.hacktricks.xyz/macos-hardening/macos-security-and-privilege-escalation/macos-apps-inspecting-debugging-and-fuzzing/introduction-to-x64
+        */
+        #define def_syscall3
+        void* syscall3(void* cn, void* a1, void* a2, void* a3) {
+            void* _ret;
+            cn = (void*) (((ulong)cn)|0x2000000ul);
+            __asm__ volatile (
+                "movq %1, %%rax\n"
+                "movq %2, %%rdi\n"
+                "movq %3, %%rsi\n"
+                "movq %4, %%rdx\n"
+                "syscall\n"
+                "movq %%rax, %0\n"
+                : "=r" (_ret)
+                : "r" (cn), "r" (a1), "r" (a2), "r" (a3)
+                : "memory", "rax", "rdi", "rsi", "rdx", "rcx", "r11"
+            );
+            return _ret;
+        }
+    #endif
+
+    #ifdef def_syscall3
+        #define def_sys_write
+        ulong _sys_write(void* buf, ulong len) {
+            return (ulong) syscall3((void*)4, (void*)1, buf, (void*)len);
+        }
+
+        #define def_sys_time
+        ulong _sys_time() {
+            ulong tt[2];
+            syscall3((void*)116, &tt, 0, 0);
+            return *tt;
+        }
+    #endif
+    
+#endif
+
+
+// #define __linux__
+#ifdef __linux__
+    #error "BROKEN: will fix later"
+    #define NEWLINE "\n"
+    
+    #ifdef __aarch64__
+        /*
+            https://www.chromium.org/chromium-os/developer-library/reference/linux-constants/syscalls/#calling-conventions
+        */
+        #define def_syscall3
+        void* syscall3(void* cn, void* a1, void* a2, void* a3) {
+            void* _ret;
+            __asm__ volatile (
+                "mov x8, %1\n"
+                "mov x0, %2\n"
+                "mov x1, %3\n"
+                "mov x2, %4\n"
+                "svc #0x80\n"
+                "mov %0, x0\n"
+                : "=r" (_ret)
+                : "r" (cn), "r" (a1), "r" (a2), "r" (a3)
+                : "memory", "x0", "x1", "x2", "x8"
+            );
+            return _ret;
+        }
+    #endif
+
+    #ifdef __x86_64__
+        /*
+            https://www.chromium.org/chromium-os/developer-library/reference/linux-constants/syscalls/#calling-conventions
         */
         #define def_syscall3
         void* syscall3(void* cn, void* a1, void* a2, void* a3) {
@@ -77,49 +143,6 @@
     #endif
     
 #endif
-
-
-// #define __linux__
-// #ifdef __linux__
-//     #define NEWLINE "\n"
-    
-//     #ifdef __aarch64__
-//         /*
-//             https://www.chromium.org/chromium-os/developer-library/reference/linux-constants/syscalls/#calling-conventions
-//         */
-//         #define def_syscall4
-//         void* syscall4(void* a1, void* a2, void* a3, void* a4) {
-//             void* _ret = 0;
-//             __asm__ volatile (
-//                 "mov x16, %1\n"
-//                 "mov x0, %2\n"
-//                 "mov x1, %3\n"
-//                 "mov x2, %4\n"
-//                 "svc #0x80\n"
-//                 "mov %0, x0\n"
-//                 : "=r" (_ret)   // output operands
-//                 : "r" (a1), "r" (a2), "r" (a3), "r" (a4) // input operands
-//                 : "x3", "x4"  // clobbers
-//             );
-//             return _ret;
-//         }
-//     #endif
-
-//     #ifdef def_syscall4
-//         #define def_sys_write
-//         ulong _sys_write(void* buf, ulong len) {
-//             return (ulong) syscall4((void*)4, (void*)1, buf, (void*)len);
-//         }
-
-//         #define def_sys_time
-//         ulong _sys_time() {
-//             ulong tt[2];
-//             syscall4((void*)116, &tt, 0, 0);
-//             return *tt;
-//         }
-//     #endif
-    
-// #endif
 
 
 // add more supported targets
